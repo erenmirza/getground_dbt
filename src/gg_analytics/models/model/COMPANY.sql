62c7757e27972aa044cc721b51090a5468ed4f3a
Eren Mirza <eren.mirza@sonovate.com>
2023-10-29 22:47:49 +0000

    updated unit test date time format

--- src/gg_analytics/models/model/COMPANY.sql
+++ src/gg_analytics/models/model/COMPANY.sql
@@ -5,16 +5,33 @@ CTE_REFERRALS_SOURCE as (
 )
 
 , CTE_DISTINCT_COMPANIES as (
-  select distinct
+  select
     COMPANY_ID
+    , max(REFERRAL_ID) as LAST_REFERRAL_ID
+    , min(REFERRAL_ID) as FIRST_REFERRAL_ID
   from CTE_REFERRALS_SOURCE
+  group by COMPANY_ID
+)
+
+, CTE_COMPANY_DETAILS as (
+  select
+    DC.COMPANY_ID
+    , R.REFERRAL_STATUS as LAST_REFERRAL_STATUS
+    , if(
+      LAST_REFERRAL_STATUS in ('successful', 'pending')
+      , 'Active'
+      , 'Inactive'
+    ) as COMPANY_STATUS
+  from CTE_DISTINCT_COMPANIES DC
+  inner join CTE_REFERRALS_SOURCE R
+  on DC.LAST_REFERRAL_ID = R.REFERRAL_ID
 )
 
 , CTE_FINAL as (
     select
       COMPANY_ID
-      {# , COMPANY_STATUS #}
-    from CTE_DISTINCT_COMPANIES
+      , COMPANY_STATUS
+    from CTE_COMPANY_DETAILS
 )
 
 select *
